e16beaa33be57ef0826f287cd62b8da35906ccdf
KIMM1CH <66040233111@udru.ac.th>
2025-02-22 23:03:53 +0700

    Add files via upload

---
+++ dbconnect.php
@@ -0,0 +1,6 @@
+<!--<link href="css/style.css" rel="stylesheet">-->
+<?php
+header('Content-Type: text/html; charset=UTF-8');
+
+$con = mysqli_connect("localhost", "its66040233111", "L1upP4Q8", "its66040233111") or die("เกิดข้อผิดพลาดเกิดขึ้น");
+mysqli_set_charset($con, "utf8");
---
+++ deletedata.php
@@ -0,0 +1,16 @@
+<?php
+header('Content-Type: text/html; charset=UTF-8');
+
+require('dbconnect.php');
+
+$emp_id = $_GET["emp_id"];
+
+$sql = "DELETE FROM employee WHERE emp_id=$emp_id";
+$result = mysqli_query($con, $sql);
+
+if ($result) {
+  header("location:index.php");
+  exit(0);
+} else {
+  echo "ไม่สามารถลบได้ หรือ มีข้อผิดพลาดเกิดขึ้น";
+}
